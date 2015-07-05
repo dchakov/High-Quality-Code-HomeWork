3bb1f8a27477fa8fbf823a63065d9f5fcc3ab46c
dchakov <dchakov@yahoo.com>
2015-07-05 21:09:10 +0300

    Add task 03

---
+++ 03-Naming Identifiers/Task1.Class/BoolConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace BoolToString
+{
+    using System;
+
+    public class BoolConverter
+    {
+        private const int MaxCount = 6;
+
+        public void ConvertBoolToString(bool variable)
+        {
+            string stringVariable = variable.ToString();
+            Console.WriteLine(stringVariable);
+        }
+    }
+}
