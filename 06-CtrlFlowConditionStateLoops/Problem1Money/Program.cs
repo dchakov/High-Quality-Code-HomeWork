542790d9e798043077ff612b513e265e1fc3bf6b
dchakov <dchakov@yahoo.com>
2015-07-11 12:47:14 +0300

    ctrl flow

---
+++ 06-CtrlFlowConditionStateLoops/Problem1Money/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        uint numberNOfStudents = uint.Parse(Console.ReadLine());
+        uint numberSOfPaperSheets = uint.Parse(Console.ReadLine());
+        double pricePOfRealm = double.Parse(Console.ReadLine());
+        double rezult = 0;
+
+        rezult = numberNOfStudents*numberSOfPaperSheets;
+        rezult = (rezult / 400)*pricePOfRealm;
+
+        Console.WriteLine("{0:0.000}",rezult);
+
+    }
+}
---
+++ 06-CtrlFlowConditionStateLoops/Problem3Maslan/Maslan.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Numerics;
+class Maslan
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+        BigInteger sumOddPositions = 0;
+        BigInteger productOfSumPositions = 1;
+        int counttransform = 0;
+        BigInteger productOnFirst = 0;
+        do
+        {
+            for (int i = 1; i <= text.Length; i++)
+            {
+                for (int j = 0; j < text.Length - i; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        sumOddPositions += (char)text[j] - '0';
+                    }
+                }
+                if (sumOddPositions != 0)
+                {
+                    productOfSumPositions *= sumOddPositions;
+                }
+                sumOddPositions = 0;
+            }
+            productOnFirst = productOfSumPositions;
+            counttransform++;
+            
+            text = Convert.ToString(productOfSumPositions);
+
+            if (text.Length == 1)
+            {
+                break;
+            }
+            productOfSumPositions = 1;
+        } while (counttransform < 10);
+
+        if (counttransform < 10)
+        {
+            Console.WriteLine(counttransform);
+            Console.WriteLine(productOfSumPositions);
+        }
+        else
+        {
+            Console.WriteLine(productOnFirst);
+        }
+
+    }
+}
