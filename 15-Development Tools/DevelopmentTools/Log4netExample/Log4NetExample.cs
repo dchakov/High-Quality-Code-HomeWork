8893ae0dc29439876ef67bdbd745d1a4afb5e423
dchakov <dchakov@yahoo.com>
2015-08-18 22:43:47 +0300

    initial

---
+++ 15-Development Tools/DevelopmentTools/Log4netExample/Log4NetExample.cs
@@ -0,0 +1,23 @@
+﻿namespace Log4NetExample
+{
+    using System;
+    using log4net;
+    using log4net.Config;
+
+    public class Log4NetExample
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Log4NetExample));
+
+        public static void Main()
+        {
+            XmlConfigurator.Configure();
+            Log.Debug("Starting application");
+            for (int i = 0; i < 50; i++)
+            {
+                Log.InfoFormat("Current time is [{0}]", DateTime.Now.ToString("yyyy.MM.dd-hh.mm.ss~fff"));
+            }
+
+            Log.Debug("Ending application");
+        }
+    }
+}
